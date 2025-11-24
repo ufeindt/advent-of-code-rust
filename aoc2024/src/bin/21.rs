044393ea8b9495874dfbe280bf29ef99e1835a2c
Ulrich Feindt <ulrich@feindt.dev>
2025-11-24 21:59:32 +0100

    2024 day 22

--- aoc2024/src/bin/21.rs
+++ aoc2024/src/bin/21.rs
@@ -66,44 +66,6 @@ fn get_numerical_direction_options(start: String, end: String) -> HashSet<String
     options
 }
 
-// @cache
-// def get_numerical_direction_options(start: str, end: str) -> set[str]:
-//     keys = {
-//         "0": (1, 3),
-//         "1": (0, 2),
-//         "2": (1, 2),
-//         "3": (2, 2),
-//         "4": (0, 1),
-//         "5": (1, 1),
-//         "6": (2, 1),
-//         "7": (0, 0),
-//         "8": (1, 0),
-//         "9": (2, 0),
-//         "A": (2, 3),
-//     }
-
-//     start_coords = keys[start]
-//     end_coords = keys[end]
-
-//     dx = end_coords[0] - start_coords[0]
-//     dy = end_coords[1] - start_coords[1]
-
-//     options = set()
-//     if (start_coords[0] + dx, start_coords[1]) in keys.values():
-//         options.add(
-//             (">" if dx > 0 else "<") * abs(dx)
-//             + ("v" if dy > 0 else "^") * abs(dy)
-//             + "A"
-//         )
-//     if (start_coords[0], start_coords[1] + dy) in keys.values():
-//         options.add(
-//             ("v" if dy > 0 else "^") * abs(dy)
-//             + (">" if dx > 0 else "<") * abs(dx)
-//             + "A"
-//         )
-
-//     return options
-
 #[memoize]
 fn get_directional_keypress_count(start: String, end: String, n_robots: u8) -> usize {
     if n_robots == 1 {
@@ -150,45 +112,6 @@ fn get_directional_keypress_count(start: String, end: String, n_robots: u8) -> u
     *counts.iter().min().unwrap()
 }
 
-// def get_directional_keypress_count(start: str, end: str, n_robots=3) -> int:
-//     if n_robots == 1:
-//         return 1
-//     keys = {
-//         "^": (1, 0),
-//         "v": (1, 1),
-//         "<": (0, 1),
-//         ">": (2, 1),
-//         "A": (2, 0),
-//     }
-
-//     start_coords = keys[start]
-//     end_coords = keys[end]
-
-//     dx = end_coords[0] - start_coords[0]
-//     dy = end_coords[1] - start_coords[1]
-//     options = set()
-//     if (start_coords[0] + dx, start_coords[1]) in keys.values():
-//         options.add(
-//             (">" if dx > 0 else "<") * abs(dx)
-//             + ("v" if dy > 0 else "^") * abs(dy)
-//             + "A"
-//         )
-
-//     if (start_coords[0], start_coords[1] + dy) in keys.values():
-//         options.add(
-//             ("v" if dy > 0 else "^") * abs(dy)
-//             + (">" if dx > 0 else "<") * abs(dx)
-//             + "A"
-//         )
-
-//     return min(
-//         sum(
-//             get_directional_keypress_count(start, end, n_robots=n_robots - 1)
-//             for start, end in zip("A" + option[:-1], option, strict=True)
-//         )
-//         for option in options
-//     )
-
 fn get_human_keypress_count(code: &str, n_robots: u8) -> usize {
     let mut keypress_options = vec![];
     for (start, end) in format!("A{}", code).chars().zip(code.chars()) {
@@ -214,23 +137,6 @@ fn get_human_keypress_count(code: &str, n_robots: u8) -> usize {
 
     total
 }
-
-// def get_human_keypress_count(code: str, n_robots: int = 3):
-//     keypress_options = [
-//         get_numerical_direction_options(start, end)
-//         for start, end in zip("A" + code[:-1], code, strict=True)
-//     ]
-
-//     total = 0
-//     for options in keypress_options:
-//         total += min(
-//             sum(
-//                 get_directional_keypress_count(start, end, n_robots=n_robots)
-//                 for start, end in zip("A" + option[:-1], option, strict=True)
-//             )
-//             for option in options
-//         )
-//     return total
 
 fn solve(prefix: Option<&str>, suffix: Option<&str>, n_robots: u8) -> usize {
     let codes = load_data(prefix, suffix);
---
+++ aoc2024/src/bin/22.rs
@@ -0,0 +1,125 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+
+static YEAR: &str = "2024";
+static DAY: &str = "22";
+
+fn load_data(prefix: Option<&str>, suffix: Option<&str>) -> Vec<isize> {
+    let mut file_name = format!("input/{YEAR}/{DAY}.input");
+    match prefix {
+        None => (),
+        Some(p) => file_name = format!("{p}{file_name}"),
+    }
+    match suffix {
+        None => (),
+        Some(s) => file_name = format!("{file_name}{s}"),
+    }
+
+    let input =
+        fs::read_to_string(Path::new(&file_name)).expect("Should have been able to read the file");
+
+    input
+        .split("\n")
+        .filter(|l| l.len() > 0)
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect()
+}
+
+fn next_secret(mut secret: isize) -> isize {
+    secret ^= secret << 6;
+    secret %= 1 << 24;
+    secret ^= secret >> 5;
+    secret %= 1 << 24;
+    secret ^= secret << 11;
+    secret %= 1 << 24;
+
+    secret
+}
+
+fn solve_part_1(prefix: Option<&str>, suffix: Option<&str>) -> isize {
+    let secrets = load_data(prefix, suffix);
+    secrets
+        .iter()
+        .map(|secret| (0..2000).fold(*secret, |acc, _| next_secret(acc)))
+        .sum()
+}
+
+fn solve_part_2(prefix: Option<&str>, suffix: Option<&str>) -> isize {
+    let secrets = load_data(prefix, suffix);
+    let mut sequence_outcomes = vec![];
+
+    for secret in secrets {
+        let mut old_secret = secret;
+        let mut sequences = HashMap::new();
+        let mut current_sequence = (0, 0, 0, 0);
+        for k in 0..2000 {
+            let new_secret = next_secret(old_secret);
+            current_sequence = (
+                current_sequence.1,
+                current_sequence.2,
+                current_sequence.3,
+                (new_secret % 10) - (old_secret % 10),
+            );
+            // println!("{k}: {:?}", current_sequence);
+            if k >= 3 && !sequences.contains_key(&current_sequence) {
+                sequences.insert(current_sequence, new_secret % 10);
+            }
+            old_secret = new_secret;
+        }
+        sequence_outcomes.push(sequences);
+    }
+
+    let all_sequences = sequence_outcomes
+        .iter()
+        .flat_map(|sequences| sequences.keys())
+        .collect::<std::collections::HashSet<_>>();
+
+    all_sequences
+        .iter()
+        .map(|sequence| {
+            sequence_outcomes
+                .iter()
+                .map(|outcomes| outcomes.get(sequence).unwrap_or(&0))
+                .sum()
+        })
+        .max()
+        .unwrap()
+}
+
+fn main() {
+    let answer1 = solve_part_1(None, None);
+    println!("Answer for part 1: {answer1}");
+
+    let answer2 = solve_part_2(None, None);
+    println!("Answer for part 2: {answer2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_part_1() {
+        let result = solve_part_1(Some("../"), Some(".example"));
+        assert_eq!(result, 37327623);
+    }
+
+    #[test]
+    fn answer_part_1() {
+        let result = solve_part_1(Some("../"), None);
+        assert_eq!(result, 14273043166);
+    }
+
+    #[test]
+    fn example_part_2() {
+        let result = solve_part_2(Some("../"), Some(".example2"));
+        assert_eq!(result, 23);
+    }
+
+    #[test]
+    fn answer_part_2() {
+        let result = solve_part_2(Some("../"), None);
+        assert_eq!(result, 1667);
+    }
+}
