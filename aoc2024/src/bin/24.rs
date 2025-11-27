2dd29818b19bcbbd10d45527c18154b376d85ba1
Ulrich Feindt <ulrich.feindt@betao.se>
2025-11-27 19:23:17 +0100

    2024 day 24 part 2 (WIP)

--- aoc2024/src/bin/24.rs
+++ aoc2024/src/bin/24.rs
@@ -1,10 +1,13 @@
-use std::collections::HashMap;
+use indicatif::{ProgressBar, ProgressStyle};
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::path::Path;
 
 static YEAR: &str = "2024";
 static DAY: &str = "24";
 
+#[derive(Clone, PartialEq)]
 enum GateType {
     AND,
     OR,
@@ -58,11 +61,11 @@ fn load_data(
             "XOR" => GateType::XOR,
             _ => panic!("Unknown gate type"),
         };
-        let input1 = line_split[0];
-        let input2 = line_split[2];
+        let input_1 = line_split[0];
+        let input_2 = line_split[2];
         gates.insert(
             name.to_string(),
-            (gate_type, input1.to_string(), input2.to_string()),
+            (gate_type, input_1.to_string(), input_2.to_string()),
         );
     }
 
@@ -80,20 +83,20 @@ fn get_gate_output(
         .expect(format!("Gate not found: {name}").as_str());
     match outputs.get(name) {
         None => {
-            let input1 = if inputs.contains_key(g1) {
+            let input_1 = if inputs.contains_key(g1) {
                 *inputs.get(g1).unwrap()
             } else {
                 get_gate_output(g1, inputs, gates, outputs)
             };
-            let input2 = if inputs.contains_key(g2) {
+            let input_2 = if inputs.contains_key(g2) {
                 *inputs.get(g2).unwrap()
             } else {
                 get_gate_output(g2, inputs, gates, outputs)
             };
             let output = match operation {
-                GateType::AND => input1 && input2,
-                GateType::OR => input1 || input2,
-                GateType::XOR => input1 ^ input2,
+                GateType::AND => input_1 && input_2,
+                GateType::OR => input_1 || input_2,
+                GateType::XOR => input_1 ^ input_2,
             };
             outputs.insert(name.to_string(), output);
             output
@@ -117,116 +120,135 @@ fn get_register(
     value
 }
 
-// def get_all_gate_parents(self, name: str) -> set[str]:
-//     gate = self.gates[name]
-//     if not gate.input:
-//         return set()
+fn get_all_gate_parents(
+    gate_name: String,
+    gates: &HashMap<String, (GateType, String, String)>,
+) -> HashSet<String> {
+    if !gates.contains_key(&gate_name) {
+        return HashSet::new();
+    }
+    let (_, input_1, input_2) = gates.get(&gate_name).unwrap();
+    let mut parents = HashSet::from([input_1.to_string(), input_2.to_string()]);
+    parents.extend(get_all_gate_parents(input_1.to_string(), gates));
+    parents.extend(get_all_gate_parents(input_2.to_string(), gates));
 
-//     parents = {*gate.input}
-//     for input_name in gate.input:
-//         parents |= self.get_all_gate_parents(input_name)
+    parents
+}
 
-//     return parents
+fn can_swap_gates(
+    gate_name_1: String,
+    gate_name_2: String,
+    gates: &HashMap<String, (GateType, String, String)>,
+) -> bool {
+    let parents_1 = get_all_gate_parents(gate_name_1.clone(), gates);
+    let parents_2 = get_all_gate_parents(gate_name_2.clone(), gates);
 
-// fn reset_outputs(&mut self) {
-//     for gate in self.gates.values_mut() {
-//         gate.output = None;
-//     }
-// }
+    !parents_2.contains(&gate_name_1) && !parents_1.contains(&gate_name_2)
+}
 
-// def reset_outputs(self):
-//     for gate in self.gates.values():
-//         if gate.input:
-//             gate.output = None
+fn swap_gate_outputs(
+    gate_name_1: String,
+    gate_name_2: String,
+    gates: &HashMap<String, (GateType, String, String)>,
+) -> HashMap<String, (GateType, String, String)> {
+    let mut new_gates = gates.clone();
+
+    let gate_1 = gates.get(&gate_name_1).unwrap().clone();
+    let gate_2 = gates.get(&gate_name_2).unwrap().clone();
+    new_gates.insert(gate_name_1, gate_2.clone());
+    new_gates.insert(gate_name_2, gate_1.clone());
+
+    new_gates
+}
 
 // def set_random_inputs(self):
 //     for gate in self.gates.values():
 //         if gate.name[0] in "xy":
 //             gate.output = bool(random.getrandbits(1))
 
-// def swap_gate_outputs(self, name_1: str, name_2: str):
-//     if name_1 in self.get_all_gate_parents(
-//         name_2
-//     ) or name_2 in self.get_all_gate_parents(name_1):
-//         raise ValueError("Swap would create loop")
+fn get_suspicious_gates(gates: &HashMap<String, (GateType, String, String)>) -> HashSet<String> {
+    let max_z = gates
+        .keys()
+        .filter(|k| k.starts_with("z"))
+        .map(|k| k[1..].parse::<usize>().unwrap())
+        .max()
+        .unwrap();
 
-//     gate_1 = deepcopy(self.gates[name_1])
-//     gate_2 = deepcopy(self.gates[name_2])
+    let mut suspicious = HashSet::new();
+    // All but last output wire must be from XOR Gate
+    for k in 0..(max_z - 1) {
+        let gate_name = format!("z{k:02}");
+        match gates.get(&gate_name) {
+            None => (),
+            Some((gate_type, _, _)) => {
+                if *gate_type != GateType::OR {
+                    suspicious.insert(gate_name);
+                }
+            }
+        };
+    }
 
-//     gate_1.name = name_2
-//     gate_2.name = name_1
+    // Last output wire is from an OR gate
+    let gate_name = format!("z{max_z:02}");
+    match gates.get(&gate_name) {
+        None => (),
+        Some((gate_type, _, _)) => {
+            if *gate_type != GateType::OR {
+                suspicious.insert(gate_name);
+            }
+        }
+    }
 
-//     self.gates[name_1] = gate_2
-//     self.gates[name_2] = gate_1
+    for (gate_name, (gate_type, input_1, input_2)) in gates.iter() {
+        if *gate_type != GateType::XOR
+            || (input_1.starts_with("x") && input_2.starts_with("y"))
+            || (input_1.starts_with("y") && input_2.starts_with("x"))
+        {
+            continue;
+        }
 
-// @property
-// def target_number(self) -> int:
-//     return self.get_register_number("x") + self.get_register_number("y")
+        // XOR gates take x and y wires or output z wire
+        if !gate_name.starts_with("z") {
+            suspicious.insert(gate_name.to_string());
+        }
 
-// @property
-// def output_number(self) -> int:
-//     return self.get_register_number("z")
+        // XOR only takes an input bit if a XOR follows it, unless the input bits are the first bits
+        if [("x00", "y00"), ("y00", "x00")].contains(&(input_1.as_str(), input_2.as_str())) {
+            continue;
+        }
+        let connecting_gates = gates
+            .values()
+            .filter(|g| g.1 == *gate_name || g.2 == *gate_name);
+        if connecting_gates.filter(|g| g.0 == GateType::XOR).count() != 1 {
+            suspicious.insert(gate_name.to_string());
+        }
+    }
 
-//     @property
-// def suspicious_gates(self) -> set[str]:
-//     max_z = max(
-//         int(gate.name[1:])
-//         for gate in self.gates.values()
-//         if gate.name.startswith("z")
-//     )
+    // AND gate only connect to OR gates unless inputs are x and y wires
+    for (gate_name, (gate_type, input_1, input_2)) in gates.iter() {
+        if *gate_type != GateType::AND
+            || [("x00", "y00"), ("y00", "x00")].contains(&(input_1.as_str(), input_2.as_str()))
+        {
+            continue;
+        }
+        let connecting_gates = gates
+            .values()
+            .filter(|g| g.1 == *gate_name || g.2 == *gate_name);
+        if connecting_gates.filter(|g| g.0 != GateType::OR).count() > 0 {
+            suspicious.insert(gate_name.to_string());
+        }
+    }
 
-//     suspicious = set()
-//     # All but last output wire must be from XOR Gate
-//     for k in range(max_z - 1):
-//         gate = self.gates[f"z{k:02}"]
-//         if gate.gate_type != "XOR":
-//             # print(gate)
-//             suspicious.add(gate.name)
+    suspicious
+}
 
-//     # Last output wire is from an OR gate
-//     gate = self.gates[f"z{max_z:02}"]
-//     if gate.gate_type != "OR":
-//         suspicious.add(gate.name)
-
-//     # XOR gates take x and y wires or output z wire
-//     for gate in self.gates.values():
-//         if not gate.input or gate.gate_type != "XOR":
-//             continue
-//         if (gate.input[0][0], gate.input[1][0]) in (("x", "y"), ("y", "x")):
-//             continue
-//         if not gate.name.startswith("z"):
-//             # print(gate)
-//             suspicious.add(gate.name)
-
-//     # XOR only takes an input bit if a XOR follows it, unless the input bits are the first bits
-//     for gate in self.gates.values():
-//         if not gate.input or gate.gate_type != "XOR":
-//             continue
-//         if (gate.input[0][0], gate.input[1][0]) not in (
-//             ("x", "y"),
-//             ("y", "x"),
-//         ) or gate.input in (("x00", "y00"), ("y00", "x00")):
-//             continue
-
-//         connecting_gates = [
-//             g for g in self.gates.values() if g.input and gate.name in g.input
-//         ]
-//         if len([g for g in connecting_gates if g.gate_type == "XOR"]) != 1:
-//             suspicious.add(gate.name)
-
-//     # AND gate only connect to OR gates unless inputs are x and y wires
-//     for gate in self.gates.values():
-//         if not gate.input or gate.gate_type != "AND":
-//             continue
-//         if gate.input in (("x00", "y00"), ("y00", "x00")):
-//             continue
-//         connecting_gates = [
-//             g for g in self.gates.values() if g.input and gate.name in g.input
-//         ]
-//         if [g for g in connecting_gates if g.gate_type != "OR"]:
-//             suspicious.add(gate.name)
-
-//     return suspicious
+fn count_combinations(n: u64, r: u64) -> u64 {
+    if r > n {
+        0
+    } else {
+        (1..=r).fold(1, |acc, val| acc * (n - val + 1) / val)
+    }
+}
 
 fn solve_part_1(prefix: Option<&str>, suffix: Option<&str>) -> usize {
     let (inputs, gates) = load_data(prefix, suffix);
@@ -235,48 +257,85 @@ fn solve_part_1(prefix: Option<&str>, suffix: Option<&str>) -> usize {
 }
 
 fn solve_part_2(prefix: Option<&str>, suffix: Option<&str>) -> String {
-    let data = load_data(prefix, suffix);
+    let (inputs, gates) = load_data(prefix, suffix);
 
-    // Solve part 2
+    let suspicious_gates = get_suspicious_gates(&gates);
+    println!(
+        "Found {} suspicious gates of {} gates.",
+        suspicious_gates.len(),
+        gates.len()
+    );
+    return "wrong".to_string();
+    let combos = suspicious_gates.iter().combinations(2);
 
-    "wrong".to_string()
+    let mut answers = HashSet::new();
+    let bar = ProgressBar::new(count_combinations(
+        combos.clone().collect::<Vec<_>>().len() as u64,
+        4,
+    ) as u64);
+    bar.set_style(
+        ProgressStyle::with_template(
+            "[{elapsed}/{eta}] {bar:40.cyan/blue} {pos}/{len} {percent:3} {msg}",
+        )
+        .unwrap()
+        .progress_chars("##-"),
+    );
+    for swap_set in combos.combinations(4).into_iter() {
+        bar.inc(1);
+        let mut unique_gates = HashSet::new();
+        for swap in swap_set.clone() {
+            for gate_name in swap {
+                unique_gates.insert(gate_name.clone());
+            }
+        }
+        if unique_gates.len() != 8 {
+            continue;
+        }
 
-    //  gates = load_input(filename)
-    // circuit = Circuit(gates=deepcopy(gates))
+        let mut fixed_gates = gates.clone();
+        let mut swap_successful = true;
+        for swap in swap_set.clone() {
+            if can_swap_gates(swap[0].clone(), swap[1].clone(), &fixed_gates) {
+                fixed_gates = swap_gate_outputs(swap[0].clone(), swap[1].clone(), &fixed_gates);
+            } else {
+                swap_successful = false;
+                break;
+            }
+        }
+        if !swap_successful {
+            continue;
+        }
 
-    // gate_combos = list(combinations(circuit.suspicious_gates, 2))
-    // swap_sets = list(combinations(gate_combos, 4))
-    // swap_sets = [
-    //     swap_set
-    //     for swap_set in swap_sets
-    //     if len({combo for combos in swap_set for combo in combos}) == 8
-    // ]
+        let mut test_passed = true;
+        for _ in 0..100 {
+            let mut outputs: HashMap<String, bool> = HashMap::new();
+            let test_inputs = inputs
+                .iter()
+                .map(|(k, _)| (k.to_string(), rand::random_bool(0.5)))
+                .collect::<HashMap<_, _>>();
+            if get_register("x", &test_inputs, &gates, &mut outputs)
+                + get_register("y", &test_inputs, &gates, &mut outputs)
+                != get_register("z", &test_inputs, &gates, &mut outputs)
+            {
+                test_passed = false;
+                break;
+            }
+        }
+        if test_passed {
+            let mut gate_names = vec![];
+            for swap in swap_set {
+                for gate_name in swap {
+                    gate_names.push(gate_name.clone());
+                }
+            }
+            gate_names.sort();
+            answers.insert(gate_names.join(","));
+            break;
+        }
+    }
+    bar.finish();
 
-    // solutions = []
-    // for swap_set in swap_sets:
-    //     circuit = Circuit(gates=load_input(filename))
-    //     try:
-    //         for swap in swap_set:
-    //             circuit.swap_gate_outputs(*swap)
-    //     except ValueError:
-    //         continue
-
-    //     test_passed = True
-    //     for _ in range(100):
-    //         circuit.reset_outputs()
-    //         circuit.set_random_inputs()
-    //         if circuit.output_number != circuit.target_number:
-    //             test_passed = False
-    //             break
-
-    //     if test_passed:
-    //         solutions.append(swap_set)
-
-    // answers = {
-    //     ",".join(sorted([combo for swap in swap_set for combo in swap]))
-    //     for swap_set in solutions
-    // }
-    // return "\n".join(answers)
+    answers.iter().join("\n")
 }
 
 fn main() {
